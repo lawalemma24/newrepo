dad627d96534f9acd4f615e4e213e7cb73d730bb
lawalemma24 <135716271+lawalemma24@users.noreply.github.com>
2025-05-13 17:35:54 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()] // Add this configuration
-    }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
-  base: './'
+  plugins: [
+    tailwindcss(),
+  ],
 })
